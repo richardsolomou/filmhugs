edc9d4bc5b6e3355b5f8bd427fd3f443285cbbc1
Richard Solomou <rsolomou@gmail.com>
2013-04-05 23:11:39 +0100

    Committed original files.

---
+++ template.hs
@@ -0,0 +1,43 @@
+-- 
+-- 
+-- MATHFUN - DISCRETE MATHEMATICS AND FUNCTIONAL PROGRAMMING
+-- Functional Programming Assignment, 2012/13
+--
+-- Template for the assignment program (replace this comment)
+-- Add your student number
+--
+
+--
+-- Types
+--
+-- Define Film type here 
+
+testDatabase :: [Film]
+testDatabase = [ ... the 25 Film values ... ]
+
+-- 
+--
+--  Your functional code goes here
+--
+--
+
+-- Demo function to test basic functionality (without persistence - i.e. 
+-- testDatabase doesn't change and nothing is saved/loaded to/from file).
+
+--demo :: Int -> IO ()
+--demo 1  = putStrLn all films after adding 2013 film "The Great Gatsby"
+-- starring "Leonardo DiCaprio" and "Tobey Maguire" to testDatabase
+--demo 2  = putStrLn (fnToTurnAListOfFilmsIntoAMultiLineString testDatabase)
+--demo 3  = putStrLn all films from 2012
+--demo 4  = putStrLn all films that "Zoe" is a fan of
+--demo 5  = putStrLn all "Tom Hanks" films from 2000 until 2011
+--demo 6  = putStrLn all films after "Zoe" becomes fan of "Forrest Gump"
+--demo 61 = putStrLn all films after "Zoe" becomes fan of "Inception"
+--demo 7  = putStrLn best "Tom Hanks" film
+--demo 8  = putStrLn top 5 films
+
+--
+--
+-- Your user interface code goes here
+--
+--
