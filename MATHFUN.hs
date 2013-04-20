e03ab4521d48db3962fea00ec88039f824434eaf
Richard Solomou <rsolomou@gmail.com>
2013-04-20 21:42:25 +0100

    Created a function to add films at the end of the database.

--- MATHFUN.hs
+++ MATHFUN.hs
@@ -10,3 +10,12 @@
 -- Film datatype consisting of Title, Cast, Year, and Fans.
 data Film =  Film String [String] Int [String]
 			 deriving (Eq, Ord, Show, Read)
+
+
+{-|--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--|-}
+{-|--#--#--#--#--#--#  FUNCTIONAL  CODE  #--#--#--#--#--#--|-}
+{-|--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--|-}
+
+-- Adds a new film at the end of the list of films provided.
+fnAddNewFilm :: Film -> [Film] -> [Film]
+fnAddNewFilm filmToAdd listOfFilms = listOfFilms ++ [filmToAdd]
